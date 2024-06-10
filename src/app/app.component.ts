bb673d98e60e14afaf23c60b115c4051fc3f373f
Hardik Gondalia <hardikgondalia1990@gmail.com>
2024-06-10 16:19:07 +0530

    logout button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './auth/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TodoService } from './todo/todo.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +12,23 @@ export class AppComponent  implements OnInit {
   title = 'todo';
 
   public showLoader:boolean = true;
+  public userLoggedIn:boolean = false;
+  constructor(private router:Router, public authService:AuthService, private todoService:TodoService){
+      }
 
   ngOnInit(): void {
     setTimeout(()=>{
       this.showLoader = false;
      },3000)
+
+     this.authService.isLoggedIn$.subscribe((res) => {
+      this.userLoggedIn = res
+    })
+
+}
+
+  logout(){
+    this.authService.logout();
+    this.userLoggedIn = false;
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TodoService } from '../todo/todo.service';
 import { BehaviorSubject, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AuthService {
   private _isLoggedIn$ = new BehaviorSubject<boolean>(false);
   isLoggedIn$ = this._isLoggedIn$.asObservable()
 
-  constructor(private todoService:TodoService) {
+  constructor(private todoService:TodoService, private router:Router) {
     const token = localStorage.getItem('token')
     this._isLoggedIn$.next(!!token)
    }
@@ -18,10 +19,17 @@ export class AuthService {
   login(email:string, password:string){
     return this.todoService.postLogin(email, password).pipe(
       tap((response:any) => {
-        this._isLoggedIn$.next(true);
-        console.log(JSON.stringify(response.responseData))
-        localStorage.setItem('token', JSON.stringify(response.responseData))
+        if(response.responseData !== null){
+          this._isLoggedIn$.next(true)
+          localStorage.setItem('token', JSON.stringify(response.responseData))
+        }
+
       })
     )
+  }
+
+  logout(){
+    localStorage.clear();
+    this.router.navigate(['/auth'])
   }
 }
